179335097608302d34c696f20092aff080ce43ef
Apisit Maneerat <mixko@mixkoap.com>
2022-01-13 16:10:44 +0700

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Mixko on 13/1/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
